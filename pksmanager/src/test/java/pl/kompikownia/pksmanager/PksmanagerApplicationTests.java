a3e8a4e7768e1ebd2e0894f71c0020de545eb342
Karol Ślusarz <karol221-10@hotmail.com>
2020-03-15 16:59:56 +0100

    First commit.

---
+++ pksmanager/src/test/java/pl/kompikownia/pksmanager/PksmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kompikownia.pksmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PksmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
